47b51649330b83d1a3b6b76aa78ed803ec78aa73
Ayomide Oluwadiya <victordiya90@gmail.com>
2023-08-29 11:34:49 +0100

    Project Template

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("link");
+mongoose
+  .connect("link")
+  .then(() => console.log("db connection is successful"))
+  .catch((err) => console.log("db connection is unsuccessful " + err));
 
 app.listen(8000, () => {
   console.log("server is running");
